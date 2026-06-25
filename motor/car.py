84f95fb5c68e1196b85e81819d51d65041c12973
Kai7Z <zkaiqi8@gmail.com>
2026-06-25 11:13:59 +0900

    Implement diagonal and sideways movement functions
    Added diagonal and sideways movement functions to control the motor.

--- motor/car.py
+++ motor/car.py
@@ -4,27 +4,57 @@ import time
 drive = MotorController()
 
 def left_turn():
-  drive.set_motor_direction(front_left=0,rear_left=0,front_right=-1,rear_right=-1)
-  drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
+        drive.set_motor_direction(front_left=0,rear_left=0,front_right=-1,rear_right=-1)
+
+        drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
 
 def stop():
-  drive.set_motors(front_left=0,rear_left=0,front_right=0,rear_right=0)
+        drive.set_motors(front_left=0,rear_left=0,front_right=0,rear_right=0)
 
 def right_turn():
-  drive.set_motor_direction(front_left=-1,rear_left=-1,front_right=0,rear_right=0)
+        drive.set_motor_direction(front_left=-1,rear_left=-1,front_right=0,rear_right=0)
 
-  drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
+        drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
 
 def forward():
-  drive.set_motor_direction(front_left=-1,rear_left=-1,front_right=-1,rear_right=-1)
+        drive.set_motor_direction(front_left=-1,rear_left=-1,front_right=-1,rear_right=-1)
 
-  drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
+        drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
 
 def backward():
-  drive.set_motor_direction(front_left=1,rear_left=1,front_right=1,rear_right=1)
+        drive.set_motor_direction(front_left=1,rear_left=1,front_right=1,rear_right=1)
 
-  drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
+        drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
 
+def diagonal_L():
+        drive.set_motor_direction(front_left=-1,rear_left=0,front_right=0,rear_right=-1)
+
+        drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
+
+def diagonal_R():
+        drive.set_motor_direction(front_left=0,rear_left=-1,front_right=-1,rear_right=0)
+
+        drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
+
+def diagonal_BL():
+        drive.set_motor_direction(front_left=1,rear_left=0,front_right=0,rear_right=1)
+
+        drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
+
+def diagonal_BR():
+        drive.set_motor_direction(front_left=0,rear_left=1,front_right=1,rear_right=0)
+
+        drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
+
+def sideway_R():
+        drive.set_motor_direction(front_left=-1,rear_left=1,front_right=1,rear_right=-1)
+
+        drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
+
+def sideway_L():
+        drive.set_motor_direction(front_left=1,rear_left=-1,front_right=-1,rear_right=1)
+
+        drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
 
 left_turn()
 time.sleep(1)
@@ -33,6 +63,18 @@ time.sleep(1)
 right_turn()
 time.sleep(1)
 backward()
+time.sleep(1)
+diagonal_L()
+time.sleep(1)
+diagonal_R()
+time.sleep(1)
+diagonal_BL()
+time.sleep(1)
+diagonal_BR()
+time.sleep(1)
+sideway_R()
+time.sleep(1)
+sideway_L()
 time.sleep(1)
 stop()
 drive.close()
