7b3453bafed7247cc6632eb977ece8057679921a
Kai7Z <zkaiqi8@gmail.com>
2026-06-25 09:32:03 +0900

    Add motor control functions for car movements
    Implement basic motor control functions for a car.

---
+++ motor/car.py
@@ -0,0 +1,38 @@
+from motor import MotorController
+import time
+
+drive = MotorController()
+
+def left_turn():
+  drive.set_motor_direction(front_left=0,rear_left=0,front_right=-1,rear_right=-1)
+  drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
+
+def stop():
+  drive.set_motors(front_left=0,rear_left=0,front_right=0,rear_right=0)
+
+def right_turn():
+  drive.set_motor_direction(front_left=-1,rear_left=-1,front_right=0,rear_right=0)
+
+  drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
+
+def forward_move():
+  drive.set_motor_direction(front_left=-1,rear_left=-1,front_right=-1,rear_right=-1)
+
+  drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
+
+def backward_move():
+  drive.set_motor_direction(front_left=1,rear_left=1,front_right=1,rear_right=1)
+
+  drive.set_motors(front_left=1000,rear_left=1000,front_right=1000,rear_right=1000)
+
+
+left_turn()
+time.sleep(1)
+forward_move()
+time.sleep(1)
+right_turn()
+time.sleep(1)
+backward_move()
+time.sleep(1)
+stop()
+drive.close()
